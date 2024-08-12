b9791db184eb60fa955cd9761e0246f4f1eb493f
khachhoangthienn <tiendat29082003@gmail.com>
2024-08-12 16:35:05 +0700

    del constraint

--- backend/src/main/java/com/koala/backend/controller/UserController.java
+++ backend/src/main/java/com/koala/backend/controller/UserController.java
@@ -4,10 +4,7 @@ import com.koala.backend.dto.request.UserCreationRequest;
 import com.koala.backend.entity.User;
 import com.koala.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/users")
 @RestController
@@ -19,4 +16,9 @@ public class UserController {
   User createUser(@RequestBody UserCreationRequest request) {
     return userService.createUser(request);
   }
+
+ @GetMapping("/{userId}")
+  User getUser(@PathVariable String userId) {
+    return userService.getUser(userId);
+ }
 }
